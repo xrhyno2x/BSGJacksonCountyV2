c44c7599d4438394959315b847514b874a3c3813
Ryanblm <ryanblm@aol.com>
2017-10-11 13:48:11 +0100

    spawn menu updated

--- config/Config_SpawnPoints.hpp
+++ config/Config_SpawnPoints.hpp
@@ -10,117 +10,141 @@
 */
 class CfgSpawnPoints {
 
+  class JacksonCounty {
+      class Civilian {
+          class lakesidevalley {
+              displayName = "LakeSide Valley";
+              spawnMarker = "civ_spawn_1";
+              icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+              conditions = "";
+          };
+          class losdiablos {
+              displayName = "Los Diablos";
+              spawnMarker = "civ_spawn_2";
+              icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+              conditions = "";
+          };
+          class morrisontown {
+              displayName = "Morrison Town";
+              spawnMarker = "civ_spawn_3";
+              icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+              conditions = "";
+          };
+      };
+      class Cop {
+          class LVPD {
+              displayName = "LakeSide Valley PD";
+              spawnMarker = "cop_spawn_1";
+              icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+              conditions = "";
+          };
+          class LDPD {
+              displayName = "Los Diablos PD";
+              spawnMarker = "cop_spawn_4";
+              icon = "\a3\ui_f\data\map\Markers\NATO\b_air.paa";
+              conditions = "license_cop_cAir";
+          };
+          class DOC {
+              displayName = "JC Maximum Security";
+              spawnMarker = "cop_spawn_2";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "";
+          };
+          class MTSO {
+              displayName = "JC Sheriff Department";
+              spawnMarker = "cop_spawn_3";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "";
+          };
+      };
+      class Medic {
+          class Lakehos {
+              displayName = "Lakeside Hospital";
+              spawnMarker = "medic_spawn_1";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "";
+          };
+          class morrhos {
+              displayName = "Morrison Regional";
+              spawnMarker = "medic_spawn_2";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "license_cop_mAir";
+          };
+          class diabhos {
+              displayName = "Los Diablos Trauma Center";
+              spawnMarker = "medic_spawn_3";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "license_cop_mAir";
+          };
+      };
+  };
     class Altis {
-        class Civilian {
-            class Kavala {
-                displayName = "Kavala";
-                spawnMarker = "civ_spawn_1";
-                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-                conditions = "!license_civ_rebel";
-            };
-
-            class Athira {
-                displayName = "Athira";
-                spawnMarker = "civ_spawn_3";
-                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-                conditions = "";
-            };
-
-            class Pyrgos {
-                displayName = "Pyrgos";
-                spawnMarker = "civ_spawn_2";
-                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-                conditions = "";
-            };
-
-            class Sofia {
-                displayName = "Sofia";
-                spawnMarker = "civ_spawn_4";
-                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-                conditions = "";
-            };
-
-            class RebelN {
-                displayName = $STR_SP_Reb_N;
-                spawnMarker = "Rebelop";
-                icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-                conditions = "license_civ_rebel";
-            };
-
-            class RebelS {
-                displayName = $STR_SP_Reb_S;
-                spawnMarker = "Rebelop_1";
-                icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-                conditions = "license_civ_rebel";
-            };
-
-            class RebelE {
-                displayName = $STR_SP_Reb_E;
-                spawnMarker = "Rebelop_2";
-                icon = "\a3\ui_f\data\map\MapControl\bunker_ca.paa";
-                conditions = "license_civ_rebel";
-            };
-        };
-
-        class Cop {
-            class Kavala {
-                displayName = "Kavala HQ";
-                spawnMarker = "cop_spawn_1";
-                icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-                conditions = "";
-            };
-
-            class Athira {
-                displayName = "Athira HQ";
-                spawnMarker = "cop_spawn_3";
-                icon = "\a3\ui_f\data\map\MapControl\fuelstation_ca.paa";
-                conditions = "";
-            };
-
-            class Pyrgos {
-                displayName = "Pyrgos HQ";
-                spawnMarker = "cop_spawn_2";
-                icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
-                conditions = "";
-            };
-
-            class Air {
-                displayName = $STR_MAR_Police_Air_HQ;
-                spawnMarker = "cop_spawn_4";
-                icon = "\a3\ui_f\data\map\Markers\NATO\b_air.paa";
-                conditions = "call life_coplevel >= 2 && {license_cop_cAir}";
-            };
-
-            class HW {
-                displayName = $STR_MAR_Highway_Patrol;
-                spawnMarker = "cop_spawn_5";
-                icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
-                conditions = "call life_coplevel >= 3";
-            };
-        };
-
-        class Medic {
-            class Kavala {
-                displayName = $STR_SP_EMS_Kav;
-                spawnMarker = "medic_spawn_1";
-                icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-                conditions = "";
-            };
-
-            class Athira {
-                displayName = $STR_SP_EMS_Ath;
-                spawnMarker = "medic_spawn_2";
-                icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-                conditions = "";
-            };
-
-            class Pyrgos {
-                displayName = $STR_SP_EMS_Pyr;
-                spawnMarker = "medic_spawn_3";
-                icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-                conditions = "";
-            };
-        };
+      class Civilian {
+          class lakesidevalley {
+              displayName = "LakeSide Valley";
+              spawnMarker = "civ_spawn_1";
+              icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+              conditions = "";
+          };
+          class losdiablos {
+              displayName = "Los Diablos";
+              spawnMarker = "civ_spawn_2";
+              icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+              conditions = "";
+          };
+          class morrisontown {
+              displayName = "Morrison Town";
+              spawnMarker = "civ_spawn_3";
+              icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+              conditions = "";
+          };
+      };
+      class Cop {
+          class LVPD {
+              displayName = "LakeSide Valley PD";
+              spawnMarker = "cop_spawn_1";
+              icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
+              conditions = "";
+          };
+          class LDPD {
+              displayName = "Los Diablos PD";
+              spawnMarker = "cop_spawn_4";
+              icon = "\a3\ui_f\data\map\Markers\NATO\b_air.paa";
+              conditions = "license_cop_cAir";
+          };
+          class DOC {
+              displayName = "JC Maximum Security";
+              spawnMarker = "cop_spawn_2";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "";
+          };
+          class MTSO {
+              displayName = "JC Sheriff Department";
+              spawnMarker = "cop_spawn_3";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "";
+          };
+      };
+      class Medic {
+          class Lakehos {
+              displayName = "Lakeside Hospital";
+              spawnMarker = "medic_spawn_1";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "";
+          };
+          class morrhos {
+              displayName = "Morrison Regional";
+              spawnMarker = "medic_spawn_2";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "license_cop_mAir";
+          };
+          class diabhos {
+              displayName = "Los Diablos Trauma Center";
+              spawnMarker = "medic_spawn_3";
+              icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
+              conditions = "license_cop_mAir";
+          };
+      };
     };
 
     class Tanoa {
